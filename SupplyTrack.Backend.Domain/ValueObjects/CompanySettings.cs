ab117070e23e889e828a27cf7791146c7b0a150e
oussama <oussamanouhar0@gmail.com>
2025-11-06 21:34:08 +0100

    feat: add Company bounded context with VO for settings, subscription, billing

---
+++ SupplyTrack.Backend.Domain/ValueObjects/CompanySettings.cs
@@ -0,0 +1,18 @@
+﻿using SupplyTrack.Backend.Domain.Enums.Company;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SupplyTrack.Backend.Domain.ValueObjects
+{
+    public class CompanySettings
+    {
+        public OperationMode OperationMode { get; set; }
+        /// <summary>
+        ///  seuil de déclenchement auto d’approvisionnement
+        /// </summary>
+        public string AutoOrderThreshold { get; set; }
+    }
+}

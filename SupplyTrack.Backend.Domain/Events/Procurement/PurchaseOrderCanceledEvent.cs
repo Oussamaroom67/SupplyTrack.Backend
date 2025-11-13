362c4eeb698bc7e8387cb89e6fbd036c77f990ac
oussama <oussamanouhar0@gmail.com>
2025-11-13 19:23:22 +0100

    refactor(domain): update purchase order event definitions

--- SupplyTrack.Backend.Domain/Events/Procurement/PurchaseOrderCanceledEvent.cs
+++ SupplyTrack.Backend.Domain/Events/Procurement/PurchaseOrderCanceledEvent.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SupplyTrack.Backend.Domain.Events.Common;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,10 @@ using System.Threading.Tasks;
 
 namespace SupplyTrack.Backend.Domain.Events.Procurement
 {
-    internal class PurchaseOrderCancelledEvent
+    internal class PurchaseOrderCanceledEvent(int purchaseOrderId, int supplierId) : IDomainEvent
     {
+        public int PurchaseOrderId { get; } = purchaseOrderId;
+        public int SupplierId { get; } = supplierId;
+        public DateTime CancelledAt { get; } = DateTime.UtcNow;
     }
 }
